1a7f1e9043f36cb7ddf2f5530fa4778cb8702251
Sohail el Sabrouty <sohail.elsabrouty202@gmail.com>
2024-03-28 14:40:48 +0100

    WIP: Updated scraper
    to account for different types of scraping regarding the desired information

--- src/main.py
+++ src/main.py
@@ -4,27 +4,11 @@ import os
 
 # Run scraper
 if __name__ == "__main__":
-    # # Start the selenium server in the background
-    # hub = subprocess.Popen(['java', '-jar', 'selenium-server-4.18.1.jar',
-    #                        'standalone','--log-level', 'OFF' 
-    #                         ])
-    # # Wait for a while to make sure the server has time to start
-    # time.sleep(5)
-
-    # Print the test phrase
-    # print("Selenium Grid started at http://localhost:4444")
-
-    start_time = time.time()
     # Execute EuroPagesScraper
-    EuroPagesProductsScraper().scrape_and_export()
+    EuroPagesProductsScraper().scrape_and_export("company_scraper", None, None)
 
     # Execute KvKNumberScraper
     # KvKNumberScraper().scrape()
 
     # Execute SMECompaniesScraper
     # SMECompaniesScraper().scrape()
-    # Wait for a while to make sure the server has time to start
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    # hub.terminate()
-    print(f"Elapsed run time: {elapsed_time} seconds")
--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 import chromedriver_autoinstaller
 import pandas as pd
 import numpy as np
+import validators
 import unidecode
 import requests
 import hashlib
@@ -49,12 +50,7 @@ class EuroPagesProductsScraper():
     """
 
     def __init__(self):
-        self.base_url = "https://www.europages.co.uk/{0}/{1}.html"
-        self.updated_products_file = "../output_data/updated_products.csv"
-        with open(self.updated_products_file, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["products_and_services", "id"])
-            file.close()
+        self.base_url = "https://www.europages.co.uk/{}/{}.html"
 
     def cookie_handler(self, provided_driver):
         # accept the cookies and then wait a little bit
@@ -67,26 +63,29 @@ class EuroPagesProductsScraper():
     def get_driver(self):
         options = webdriver.ChromeOptions() 
         options.add_argument("--headless") # Set the Chrome webdriver to run in headless mode for 
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-dev-shm-using")
+        options.add_argument("disable-infobars")
+        options.add_argument('--blink-settings=imagesEnabled=false')
         driver = webdriver.Chrome(options=options)
         return driver
     
     # Function to generate a unique ID for each product name using a hash function
-    def generate_product_id(self, product_name):
-        return hashlib.md5(product_name.encode()).hexdigest()
+    def generate_hash_id(self, name):
+        return hashlib.md5(name.encode()).hexdigest()
     
-    def extract_products_and_services(self, link: str, company_id: str):
+    def extract_products_and_services(self, out: string, link: string):
+        company_id = link.split("/")[-2].lower()+ "_" + link.split("/")[-1].split(".")[0]
         current_driver = self.get_driver()
         current_driver.get(link)
         self.cookie_handler(current_driver)
 
         # check if there is a particular element that needs to be clicked to reveal more products
         try:
-            WebDriverWait(current_driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//section[@class='ep-keywords ep-page-epage-home__order-other pb-4']/button")))
+            WebDriverWait(current_driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//section[@class='ep-keywords ep-page-epage-home__order-other pb-4']/button")))
             current_driver.find_element(By.XPATH,"//section[@class='ep-keywords ep-page-epage-home__order-other pb-4']/button").click()
-            print("Webpage with more products and services found.")
         except Exception as e:
-            return
+            pass
 
         # wait 2 seconds to simulate human behavior
         time.sleep(2)
@@ -96,33 +95,282 @@ class EuroPagesProductsScraper():
         activities = [activity.text.strip() for activity in soup.find("ul", class_="ep-keywords__list pl-0").find_all("li")]  
         # merge the list together separate by |
         activities = ' | '.join(activities).lower()
-        with open(self.updated_products_file, 'a') as file:
+
+        with open(out, 'a') as file:
             writer = csv.writer(file)
             writer.writerow([activities, company_id])
             file.close()
         current_driver.quit()
 
-    def scrape_and_export(self):
-        # first obtain a list of all the companies_ids
-        companies_t = pd.read_csv("../input_data/products.csv")["id"].tolist()
-        # splitted_ids = np.array_split(companies_t,os.cpu_count())
-        companies = [company.upper() for company in companies_t]
-        links = np.array([self.base_url.format(*company.split("_")) for company in companies])
-        # split the list by the number of cores
-        # splitted_links = np.array_split(links, os.cpu_count())
+    def extract_ep_categories_and_sectors(self, out: string, link: string):
+        current_driver = self.get_driver()
+        current_driver.get(link)
+        self.cookie_handler(current_driver)
 
+        try:
+            WebDriverWait(current_driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "ep-show-more-less")))
+            buttons = current_driver.find_elements(By.CLASS_NAME,"ep-show-more-less")
+            for button in buttons:
+                button.click()
+        except Exception as e:
+            pass
+
+        # wait 2 seconds to simulate human behavior
+        time.sleep(2)
+        # then scrape from the page
+        soup = BeautifulSoup(current_driver.page_source, "html.parser")
+
+        # get the categories
+        cards = soup.find_all("li", class_="ep-business-sector-item")
+        for card in cards:
+            category = " ".join(card.find("span", class_="ep-card-title__text").text.strip().split())
+            sectors = card.find_all("a", class_="ep-link-list")
+            for sector in sectors:
+                with open(out, 'a') as file:
+                    writer = csv.writer(file)
+                    writer.writerow([category, " ".join(sector.text.strip().split())])
+                    file.close()
+        current_driver.quit()
+
+    def extract_ep_subsectors(self, out: string, link: string):
+
+        current_driver = self.get_driver()
+        current_driver.get(link)
+
+        self.cookie_handler(current_driver)
+
+        soup = BeautifulSoup(current_driver.page_source, "html.parser")
+
+        # get the subsectors
+        subsectors = soup.find_all("a", class_="ep-link-list px-4 py-3 text-decoration-none d-flex")
+        for subsector in subsectors:
+            # print(subsector.text.strip())
+            category = " ".join(soup.find_all("a", class_="v-breadcrumbs__item")[-1].text.strip().split())
+            sector = " ".join(soup.find("div", class_="v-breadcrumbs__item").text.strip().split())
+            with open(out, 'a') as file:
+                writer = csv.writer(file)
+                writer.writerow([self.generate_hash_id(subsector.text.strip()), re.sub('[^0-9a-zA-Z]+', "_", category.lower()), re.sub('[^0-9a-zA-Z]+', "_", sector.lower()), re.sub('[^0-9a-zA-Z]+', "_", subsector.text.strip().lower()), "https://www.europages.co.uk/{}".format(subsector["href"])])
+                file.close()
+
+        current_driver.quit()
+
+    def extract_company_urls(self, out: string, link: string):
+        current_driver = self.get_driver()
+        current_driver.get(link)
+
+        self.cookie_handler(current_driver)
+        last_page_reached = False   
+        # try to click the next page button
+        # while not last_page_reached:
+        while not last_page_reached:
+            try:
+                soup = BeautifulSoup(current_driver.page_source, "html.parser")
+                # get the company urls
+                company_url_final_part = soup.find_all("a", class_="ep-ecard-serp__epage-link")
+                company_urls = ["https://www.europages.co.uk" + company["href"] for company in company_url_final_part]
+                for company_url in company_urls:
+                    with open(out, 'a') as file:
+                        writer = csv.writer(file)
+                        writer.writerow([link, company_url])
+                        file.close()
+                WebDriverWait(current_driver, 4).until(EC.element_to_be_clickable((By.XPATH, "//ul[@class='ep-server-side-pagination__list text-center pl-0 pt-5']/li[last()]/a[@class='ep-server-side-pagination-item rounded ep-server-side-pagination__prev-next elevation-2']")))
+                next_page = current_driver.find_elements(By.XPATH, "//ul[@class='ep-server-side-pagination__list text-center pl-0 pt-5']/li[last()]/a[@class='ep-server-side-pagination-item rounded ep-server-side-pagination__prev-next elevation-2']")
+                current_driver.get(next_page[-1].get_attribute("href"))
+            except Exception as e:
+                print("Last page reached")
+                last_page_reached = True
+    
+    def extract_company_information(self, out: string, classification: pd.DataFrame, link: string):
+        
+        current_driver = self.get_driver()
+        current_driver.get(link)
+
+        self.cookie_handler(current_driver)
+        all_info = {"company_name": "", "group": "" , "sector": "", "subsector": "", "main_activity": "", "address": "", "company_city": "", 
+                    "postcode": "", "country": "", "products_and_services": "", "information": "", "min_headcount": "", "max_headcount": "", "type_of_building_for_registered_address": "", 
+                    "verified_by_europages": "", "year_established": "", "websites": "", "download_datetime": "", "id": "", "filename": ""}
+        
+        ## ID
+        company_id = link.split("/")[-2].lower()+ "_" + link.split("/")[-1].split(".")[0]
+        all_info["id"] = company_id
+
+        ## PRODUCT AND SERVICES
+        # check if there is a particular element that needs to be clicked to reveal more products
+        try:
+            WebDriverWait(current_driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//section[@class='ep-keywords ep-page-epage-home__order-other pb-4']/button")))
+            current_driver.find_element(By.XPATH,"//section[@class='ep-keywords ep-page-epage-home__order-other pb-4']/button").click()
+        except Exception as e:
+            pass
+
+        # wait 2 seconds to simulate human behavior
+        time.sleep(2)
+        # then scrape from the page
+        soup = BeautifulSoup(current_driver.page_source, "html.parser")
+        # get the activities
+        products_and_services = [activity.text.strip() for activity in soup.find("ul", class_="ep-keywords__list pl-0").find_all("li")]  
+        # merge the list together separate by |
+        products_and_services = ' | '.join(products_and_services).lower()
+        all_info["products_and_services"] = products_and_services
+
+        ## KEY FIGURES
+        try:
+            headcounts = soup.find("li", class_="ep-epages-business-details-headcount").find("dd").text.strip().split()
+            min_headcount = headcounts[0].strip()
+            max_headcount = headcounts[2].strip()
+            all_info["min_headcount"] = min_headcount
+            all_info["max_headcount"] = max_headcount
+        except Exception as e:
+            pass
+
+        ## COMPANY_NAME
+        company_name = soup.find("h1", class_="ep-epages-header-title").text.strip().lower()
+        all_info["company_name"] = company_name
+
+        ## SUBSECTOR
+        subsector = re.sub('[^0-9a-zA-Z]+', "_", soup.find_all("a", class_="v-breadcrumbs__item")[1].text.strip().lower())
+        all_info["subsector"] = subsector
+
+        ## SECTOR
+        row_of_relevancy = classification[classification["subsector"] == subsector]
+        sector = row_of_relevancy["sector"].values[0]
+        all_info["sector"] = sector
+        ## GROUP
+        group = row_of_relevancy["group"].values[0]
+        all_info["group"] = group
+
+        ## ORGANISATION
+        organisation = soup.find("div", class_="ep-epages-home-business-details__organization")
+
+        try:
+            year_established = organisation.find("li", class_="ep-epages-business-details-year-established").find("dd").text.strip().lower()
+            all_info["year_established"] = year_established
+        except Exception as e:
+            pass
+
+        try:
+            main_activity = organisation.find("li", class_="ep-epages-business-details-main-activity").find("dd").text.strip().lower()
+            all_info["main_activity"] = main_activity
+        except Exception as e:
+            pass
+
+        ## WEBSITES
+        try:
+            # get href of the website
+            websites = soup.find("a", class_="ep-epage-sidebar__website-button")["href"]
+            all_info["websites"] = websites
+        except Exception as e:
+            pass
+        
+        ## VERIFIED BY EUROPAGES
+        try:
+            verified_by_europages = soup.find("img", class_="ep-verified-badge")
+            all_info["verified_by_europages"] = True
+        except Exception as e:
+            pass
+
+        ## INFORMATION
+        information = soup.find("div", class_="ep-epage-home-description__text").find("p").text.strip().lower()
+        all_info["information"] = information
+        ## ADDRESS
+        location_info = soup.find("dl", class_="ep-epages-sidebar__info").find("dd").find_all("p")
+        address = location_info[0].text.strip().lower()
+        all_info["address"] = address
+        ## COUNTRY
+        country = location_info[2].text.strip().split("-")[1].strip().lower()
+        all_info["country"] = country
+        ## CITY
+        city = location_info[2].text.strip().split("-")[0].strip().split(" ", 1)[1].lower()
+        all_info["company_city"] = city
+        ## POSTCODE
+        postcode = location_info[2].text.strip().split("-")[0].strip().split(" ", 1)[0].lower()
+        all_info["postcode"] = postcode
+
+        # write all the values of the dictionary to the csv file
+        with open(out, 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow([*all_info.values()])
+            file.close()
+        
+        current_driver.quit()
+
+    def scrape_and_export(self, type="product_updater", input_file=None, country=None):
         # List of jobs to get executed
         executors_list = []
+
+        if type == "product_updater":
+            out = f"../output_data/product_updater_out_{time.time()}.csv"
+            with open(out, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["product_and_services", "id"])
+                file.close()
+            try:
+                companies_t = pd.read_csv(input_file)["id"].tolist()
+            except Exception as e:
+                raise Exception("Please provide a valid input file")
+            companies = [company.upper() for company in companies_t]
+            links = np.array([self.base_url.format(*company.split("_")) for company in companies])
+            args = [self.extract_products_and_services, out] # args is always of structure [function, *function_args]
+
+        elif type == "company_url_scraper": 
+            input = "../output_data/sohail_ep_categories.csv"
+            categorization = pd.read_csv(input)
+            # only get rows where the sector column is equal to "Agriculture - Machines & Equipment"
+            sector_urls = categorization[categorization["sector"] == "agriculture_machines_equipment"]["subsector_url"].tolist()
+            sector_urls = [sector_url.rsplit("/",1)[0]+ "/{}/".format(country) + sector_url.rsplit("/",1)[1] for sector_url in sector_urls][:1]
+
+            out = f"../output_data/company_url_scraper_out.csv"
+            with open(out, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["subsector_url", "company_url"])
+                file.close()
+            links = sector_urls
+            args = [self.extract_company_urls, out] # args is always of structure [function, *function_args]
+
+        elif type == "company_scraper":
+            input = "../output_data/company_url_scraper_out.csv"
+            classification = pd.read_csv("../output_data/sohail_ep_categories.csv")
+            company_urls = pd.read_csv(input)["company_url"].tolist()[:50]
+
+            out = f"../output_data/sohail_ep_companies.csv"
+            with open(out, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["company_name","group", "sector", "subsector", "main_activity" ,"address", "company_city", "postcode", "country", "products_and_services",
+                                 "information", "min_headcount", "max_headcount", "type_of_building_for_registered_address", "verified_by_europages", "year_established","websites"
+                                ,"download_datetime" ,"id", "filename"])
+                file.close()
+
+            links = company_urls
+            args = [self.extract_company_information, out, classification] # args is always of structure [function, *function_args]
+
+        elif type == "business_sectors_scraper":
+            out = f"../output_data/recent_business_sectors_scraper_out.csv"
+            with open(out, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["group", "sector"])
+                file.close()
+            links = ["https://www.europages.co.uk/bs"]
+            args = [self.extract_ep_categories_and_sectors, out] # args is always of structure [function, *function_args]
+
+        elif type == "categories_scraper":
+            input = "../output_data/recent_business_sectors_scraper_out.csv"
+            categories = pd.read_csv(input)["group"].tolist()
+            sectors = pd.read_csv(input)["sector"].tolist()
+            out = f"../output_data/sohail_ep_categories.csv"
+            with open(out, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["category_id", "group", "sector", "subsector", "link"])
+                file.close()
+            links = ['https://www.europages.co.uk/bs/{0}/{1}'.format(re.sub('[^0-9a-zA-Z]+', "-", categories[i].lower()), re.sub('[^0-9a-zA-Z]+', "-", sectors[i].lower())) for i in range(len(categories))]
+            args = [self.extract_ep_subsectors, out] # args is always of structure [function, *function_args]
 
         print("--- Starting scraping procedure--")
         # start timer
         start_time = time.time()
-        print("Scraping company products_and_services from EuroPages")
-        with ThreadPoolExecutor() as executor:
+        with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
             # Add the 10 concurrent tasks to scrape product data from different parts of the 'links' list
             for i in range(len(links)):
                 executors_list.append(
-                    executor.submit(self.extract_products_and_services, links[i], companies_t[i])
+                    executor.submit(*args, links[i])
                 )
 
         wait(executors_list)
